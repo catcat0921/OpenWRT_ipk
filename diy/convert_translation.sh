451a798560ac0b84685a1fcca83a6efda69cff34
kenzok8 <kenzok8@gmail.com>
2021-09-05 18:32:56 +0800

    int

--- diy/convert_translation.sh
+++ diy/convert_translation.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
-# [CTCGFW]Project-OpenWrt
-# Use it under GPLv3, please.
-# --------------------------------------------------------
-# Convert translation files zh-cn to zh_Hans
-# The script is still in testing, welcome to report bugs.
 
-po_file="$({ find luci-app* -type f -maxdepth 3 -name "*.po"; } 2>"/dev/null")"
-for a in ${po_file}
-do
-	[ -n "$(grep "Language: zh_CN" "$a")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$a"
-done
-
-po_dir="$({ find luci-app* -type d -maxdepth 2 -name "zh-cn"; } 2>"/dev/null")"
-for e in ${po_dir}
-do
-	po_new_dir="$(echo -e "$e"|sed "s/zh-cn/zh_Hans/g")"
-	mv "$e" "${po_new_dir}" 2>"/dev/null"
-done
-
-makefile_file="$({ find luci-app* -type f -maxdepth 1 -name Makefile; } 2>"/dev/null")"
-for f in ${makefile_file}
-do
-	[ -n "$(grep "zh-cn/" "$f")" ] && sed -i "s/zh-cn\//zh_Hans\//g" "$f"
-	[ -n "$(grep "zh_Hans.lmo" "$f")" ] && sed -i "s/zh_Hans.lmo/zh-cn.lmo/g" "$fn"
+for e in $(ls -d luci-*/po); do
+	if [[ -d $e/zh-cn && ! -d $e/zh_Hans ]]; then
+		ln -s zh-cn $e/zh_Hans 2>/dev/null
+	elif [[ -d $e/zh_Hans && ! -d $e/zh-cn ]]; then
+		ln -s zh_Hans $e/zh-cn 2>/dev/null
+	fi
 done
