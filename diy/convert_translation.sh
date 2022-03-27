29610ae58440fd394ddf779bc6739c8fc3bb1db4
有種 <39034242+kenzok8@users.noreply.github.com>
2022-03-27 10:33:30 +0800

    Update convert_translation.sh

--- diy/convert_translation.sh
+++ diy/convert_translation.sh
@@ -7,7 +7,7 @@ for e in $(ls -d luci-*/po); do
 		ln -s zh_Hans $e/zh-cn 2>/dev/null
 	fi
 
-for e in $(ls -d default-settings/po); do
+for e in $(ls -d default-*/po); do
 	if [[ -d $e/zh-cn && ! -d $e/zh_Hans ]]; then
 		ln -s zh-cn $e/zh_Hans 2>/dev/null
 	elif [[ -d $e/zh_Hans && ! -d $e/zh-cn ]]; then
